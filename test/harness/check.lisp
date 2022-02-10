d490bf9c73a3c40a73fec7eb513b8d6fdaf3f98a
Rich Heller <rellehr@gmail.com>
2022-02-10 17:44:18 -0600

    Token atom tests

--- test/harness/check.lisp
+++ test/harness/check.lisp
@@ -6,8 +6,45 @@
 (in-package :alive/test/harness/check)
 
 
+(defmethod deep-equal-p ((a alive/parse/pos::pos) (b alive/parse/pos::pos))
+    (and (eq (alive/parse/pos:line a) (alive/parse/pos:line b))
+         (eq (alive/parse/pos:col a) (alive/parse/pos:col b))))
+
+
+(defmethod deep-equal-p ((a alive/parse/token::token) (b alive/parse/token::token))
+    (and (deep-equal-p (alive/parse/token:start a) (alive/parse/token:start b))
+         (deep-equal-p (alive/parse/token:end a) (alive/parse/token:end b))
+         (string-equal (alive/parse/token:text a) (alive/parse/token:text b))
+         (eq (alive/parse/token:type-value a) (alive/parse/token:type-value b))))
+
+
+(defun has-deep-equal-p (obj1 obj2)
+    (find-method #'deep-equal-p
+                 '()
+                 (mapcar #'find-class
+                         (list (type-of obj1)
+                               (type-of obj2)))
+                 nil))
+
+
+(defun compare (obj1 obj2)
+    (labels ((compare-lists (obj1 obj2)
+                  (cond ((not (typep obj2 'cons)) nil)
+                        ((not (eq (length obj1) (length obj2))) nil)
+                        (t (loop :with same := t
+
+                                 :for item1 :in obj1
+                                 :for item2 :in obj2 :do
+                                     (setf same (and same (compare item1 item2)))
+
+                                 :finally (return same))))))
+
+        (cond ((has-deep-equal-p obj1 obj2) (deep-equal-p obj1 obj2))
+              ((typep obj1 'cons) (compare-lists obj1 obj2)))))
+
+
 (defun are-equal (expected actual)
-    (unless (equalp expected actual)
+    (unless (compare expected actual)
             (error 'err:test-failed
                    :expected expected
                    :actual actual)))
--- test/parse/tokens.lisp
+++ test/parse/tokens.lisp
@@ -24,7 +24,71 @@
                                           :start (pos:create :line 0 :col 0)
                                           :end (pos:create :line 0 :col 3)
                                           :text "foo"))
-                                   (token-for-string "foo")))))
+                                   (token-for-string "foo"))
+
+                  (check:are-equal (list (token:create
+                                          :type-value alive/types:*symbol*
+                                          :start (pos:create :line 0 :col 0)
+                                          :end (pos:create :line 0 :col 5)
+                                          :text "defun"))
+                                   (token-for-string "defun"))
+
+                  (check:are-equal (list (token:create
+                                          :type-value alive/types:*comment*
+                                          :start (pos:create :line 0 :col 0)
+                                          :end (pos:create :line 0 :col 9)
+                                          :text "; Comment"))
+                                   (token-for-string "; Comment"))
+
+                  (check:are-equal (list (token:create
+                                          :type-value alive/types:*string*
+                                          :start (pos:create :line 0 :col 0)
+                                          :end (pos:create :line 0 :col 8)
+                                          :text "\"String\""))
+                                   (token-for-string "\"String\""))
+
+                  (check:are-equal (list (token:create
+                                          :type-value alive/types:*macro*
+                                          :start (pos:create :line 0 :col 0)
+                                          :end (pos:create :line 0 :col 6)
+                                          :text "#'abcd"))
+                                   (token-for-string "#'abcd"))
+
+                  (check:are-equal (list (token:create
+                                          :type-value alive/types:*ifdef-false*
+                                          :start (pos:create :line 0 :col 0)
+                                          :end (pos:create :line 0 :col 3)
+                                          :text "#+n"))
+                                   (token-for-string "#+n"))
+
+                  (check:are-equal (list (token:create
+                                          :type-value alive/types:*open-paren*
+                                          :start (pos:create :line 0 :col 0)
+                                          :end (pos:create :line 0 :col 1)
+                                          :text "(")
+                                         (token:create
+                                          :type-value alive/types:*close-paren*
+                                          :start (pos:create :line 0 :col 1)
+                                          :end (pos:create :line 0 :col 2)
+                                          :text ")"))
+                                   (token-for-string "()"))
+
+                  (check:are-equal (list (token:create
+                                          :type-value alive/types:*symbol*
+                                          :start (pos:create :line 0 :col 0)
+                                          :end (pos:create :line 0 :col 3)
+                                          :text "foo")
+                                         (token:create
+                                          :type-value alive/types:*colons*
+                                          :start (pos:create :line 0 :col 3)
+                                          :end (pos:create :line 0 :col 5)
+                                          :text "::")
+                                         (token:create
+                                          :type-value alive/types:*symbol*
+                                          :start (pos:create :line 0 :col 5)
+                                          :end (pos:create :line 0 :col 8)
+                                          :text "bar"))
+                                   (token-for-string "foo::bar")))))
 
 
 (defun run-all ()
