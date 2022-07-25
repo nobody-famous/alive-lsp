b6062871c10179c2d22c00d3ac87b2514f2a81d6
Rich Heller <rellehr@gmail.com>
2022-07-24 23:02:09 -0500

    Remove compile message class

--- src/compile-message.lisp
+++ src/compile-message.lisp
@@ -1,32 +1,21 @@
 (defpackage :alive/compile-message
     (:use :cl)
     (:export :create
-             :message))
+             :get-message))
 
 (in-package :alive/compile-message)
 
 
-(defclass compile-message ()
-        ((severity :accessor severity
-                   :initform nil
-                   :initarg :severity)
-         (location :accessor location
-                   :initform nil
-                   :initarg :location)
-         (message :accessor message
-                  :initform nil
-                  :initarg :message)))
-
-
-(defmethod print-object ((obj compile-message) out)
-    (format out "{severity: ~A; location: ~A; message: ~A}"
-        (severity obj)
-        (location obj)
-        (message obj)))
+(defun get-message (obj)
+    (when obj
+          (gethash "message" obj)))
 
 
 (defun create (&key severity location message)
-    (make-instance 'compile-message
-        :severity severity
-        :location location
-        :message message))
+    (let ((obj (make-hash-table :test #'equalp)))
+
+        (setf (gethash "severity" obj) severity)
+        (setf (gethash "location" obj) location)
+        (setf (gethash "message" obj) message)
+
+        obj))
