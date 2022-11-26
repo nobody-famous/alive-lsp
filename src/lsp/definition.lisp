54159e79e8c8d09db80bef2f7115cd82becd0c54
Rich Heller <rellehr@gmail.com>
2022-11-26 11:08:21 -0600

    Code cleanup

--- src/lsp/definition.lisp
+++ src/lsp/definition.lisp
@@ -1,90 +1,16 @@
 (defpackage :alive/lsp/definition
     (:use :cl)
     (:export :get-location)
-    (:local-nicknames (:packages :alive/packages)
-                      (:utils :alive/lsp/utils)
-                      (:forms :alive/parse/forms)))
+    (:local-nicknames (:forms :alive/parse/forms)
+                      (:packages :alive/packages)
+                      (:utils :alive/lsp/utils)))
 
 (in-package :alive/lsp/definition)
 
 
-(defun get-range-from-file (file source-path)
-    (handler-case
-            (with-open-file (in-stream file)
-                (let ((forms (forms:from-stream in-stream)))
-                    (forms:get-range-for-path forms source-path)))
-        (T nil)))
-
-
-(defun needs-encoding (char)
-    (eq char #\:))
-
-
-(defun encode-char (char)
-    (if (needs-encoding char)
-        (format nil "%~2,'0X" (char-code char))
-        (string char)))
-
-
-(defun url-encode (str)
-    (let ((chars (map 'list (lambda (char)
-                                (encode-char char))
-                     str)))
-        (apply #'concatenate 'string chars)))
-
-
-(defun url-encode-filename (name)
-    (let* ((raw-pieces (uiop:split-string name :separator "/\\"))
-           (pieces (mapcar (lambda (piece)
-                               (if (string= piece "")
-                                   ""
-                                   (format NIL "/~A" (url-encode piece))))
-                           raw-pieces)))
-        (apply #'concatenate 'string pieces)))
-
-
-(defun lookup-sources (sym)
-    (let ((types (list :class
-                       :compiler-macro
-                       :condition
-                       :constant
-                       :function
-                       :generic-function
-                       :macro
-                       :method
-                       :method-combination
-                       :package
-                       :setf-expander
-                       :structure
-                       :symbol-macro
-                       :type
-                       :alien-type
-                       :variable
-                       :declaration)))
-        (reduce (lambda (out item)
-                    (if out
-                        out
-                        (let* ((srcs (sb-introspect:find-definition-sources-by-name sym item))
-                               (src (first srcs)))
-                            (if (and src
-                                     (sb-introspect:definition-source-pathname src))
-                                src
-                                nil))))
-                types
-            :initial-value nil)))
-
-
-(defun get-symbol-location (name pkg-name)
-    (let* ((sym (alive/symbols:lookup name pkg-name))
-           (src (when sym
-                      (lookup-sources sym)))
-           (file (when src (sb-introspect:definition-source-pathname src)))
-           (form-path (when src (sb-introspect:definition-source-form-path src))))
-
-        (if file
-            (list (url-encode-filename (namestring file))
-                  (get-range-from-file file form-path))
-            (list nil nil))))
+(defun get-location-for-name (name pkg-name)
+    (let ((sym (alive/symbols:lookup name pkg-name)))
+        (alive/symbols:get-location sym)))
 
 
 (defun get-location (&key text pos)
@@ -97,4 +23,4 @@
                 (utils:symbol-for-pos :text text :pos pos)
 
             (when (and name pkg-name)
-                  (get-symbol-location name pkg-name)))))
+                  (get-location-for-name name pkg-name)))))
--- src/symbols.lisp
+++ src/symbols.lisp
@@ -4,10 +4,12 @@
              :get-all
              :get-all-names
              :get-lambda-list
+             :get-location
              :external-p
              :function-p
              :lookup
-             :macro-p))
+             :macro-p)
+    (:local-nicknames (:forms :alive/parse/forms)))
 
 (in-package :alive/symbols)
 
@@ -76,3 +78,81 @@
     (let ((syms (list)))
         (do-symbols (s pkg syms)
             (push (string-downcase (string s)) syms))))
+
+
+(defun needs-encoding (char)
+    (eq char #\:))
+
+
+(defun encode-char (char)
+    (if (needs-encoding char)
+        (format nil "%~2,'0X" (char-code char))
+        (string char)))
+
+
+(defun url-encode (str)
+    (let ((chars (map 'list (lambda (char)
+                                (encode-char char))
+                     str)))
+        (apply #'concatenate 'string chars)))
+
+
+(defun url-encode-filename (name)
+    (let* ((raw-pieces (uiop:split-string name :separator "/\\"))
+           (pieces (mapcar (lambda (piece)
+                               (if (string= piece "")
+                                   ""
+                                   (format NIL "/~A" (url-encode piece))))
+                           raw-pieces)))
+        (apply #'concatenate 'string pieces)))
+
+
+(defun lookup-sources (sym)
+    (let ((types (list :class
+                       :compiler-macro
+                       :condition
+                       :constant
+                       :function
+                       :generic-function
+                       :macro
+                       :method
+                       :method-combination
+                       :package
+                       :setf-expander
+                       :structure
+                       :symbol-macro
+                       :type
+                       :alien-type
+                       :variable
+                       :declaration)))
+        (reduce (lambda (out item)
+                    (if out
+                        out
+                        (let* ((srcs (sb-introspect:find-definition-sources-by-name sym item))
+                               (src (first srcs)))
+                            (if (and src
+                                     (sb-introspect:definition-source-pathname src))
+                                src
+                                nil))))
+                types
+            :initial-value nil)))
+
+
+(defun get-range-from-file (file source-path)
+    (handler-case
+            (with-open-file (in-stream file)
+                (let ((forms (forms:from-stream in-stream)))
+                    (forms:get-range-for-path forms source-path)))
+        (T nil)))
+
+
+(defun get-location (sym)
+    (let* ((src (when sym
+                      (lookup-sources sym)))
+           (file (when src (sb-introspect:definition-source-pathname src)))
+           (form-path (when src (sb-introspect:definition-source-form-path src))))
+
+        (if file
+            (list (url-encode-filename (namestring file))
+                  (get-range-from-file file form-path))
+            (list nil nil))))
