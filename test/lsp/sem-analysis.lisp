2e37ede7e125ca2f90f0135c3a0b2b1f997aa8d3
Rich Heller <rellehr@gmail.com>
2023-07-05 17:50:02 -0500

    Analysis symbol type tests

--- test/lsp/sem-analysis.lisp
+++ test/lsp/sem-analysis.lisp
@@ -33,7 +33,69 @@
                               :actual (length (analysis::sem-tokens state))))))
 
 
+(defun test-get-type ()
+    (clue:test "Get type"
+        (let ((state (make-instance 'analysis::analysis-state)))
+            (clue:check-equal :expected nil
+                              :actual (analysis::get-symbol-type state "foo")))))
+
+
+(defun test-update-type ()
+    (clue:test "Update type 2 symbols"
+        (let ((state (make-instance 'analysis::analysis-state)))
+            (setf (analysis::lex-tokens state) (list (token:create :type-value alive/types:*symbol*
+                                                                   :start (pos:create 0 0)
+                                                                   :start-offset 0
+                                                                   :end (pos:create 0 4)
+                                                                   :end-offset 4
+                                                                   :text "text")
+                                                     (token:create :type-value alive/types:*symbol*
+                                                                   :start (pos:create 0 5)
+                                                                   :start-offset 5
+                                                                   :end (pos:create 0 6)
+                                                                   :end-offset 6
+                                                                   :text ":")))
+            (clue:check-equal :expected nil
+                              :actual (analysis::update-symbol-types state))))
+
+    (clue:test "Update type symbol and colons"
+        (let ((state (make-instance 'analysis::analysis-state)))
+            (setf (analysis::lex-tokens state) (list (token:create :type-value alive/types:*symbol*
+                                                                   :start (pos:create 0 0)
+                                                                   :start-offset 0
+                                                                   :end (pos:create 0 4)
+                                                                   :end-offset 4
+                                                                   :text "text")
+                                                     (token:create :type-value alive/types:*colons*
+                                                                   :start (pos:create 0 5)
+                                                                   :start-offset 5
+                                                                   :end (pos:create 0 6)
+                                                                   :end-offset 6
+                                                                   :text ":")))
+            (clue:check-equal :expected nil
+                              :actual (analysis::update-symbol-types state))))
+
+    (clue:test "Update type no symbols"
+        (let ((state (make-instance 'analysis::analysis-state)))
+            (setf (analysis::lex-tokens state) (list (token:create :type-value alive/types:*colons*
+                                                                   :start (pos:create 0 0)
+                                                                   :start-offset 0
+                                                                   :end (pos:create 0 4)
+                                                                   :end-offset 4
+                                                                   :text ":")
+                                                     (token:create :type-value alive/types:*colons*
+                                                                   :start (pos:create 0 5)
+                                                                   :start-offset 5
+                                                                   :end (pos:create 0 6)
+                                                                   :end-offset 6
+                                                                   :text ":")))
+            (clue:check-equal :expected nil
+                              :actual (analysis::update-symbol-types state)))))
+
+
 (defun run-all ()
     (clue:suite "Semantic Analysis Tests"
         (test-is-number)
-        (test-add-token)))
+        (test-add-token)
+        (test-get-type)
+        (test-update-type)))
