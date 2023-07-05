0bfe884b476a4fbdccb53b19aafe196719585fe3
Rich Heller <rellehr@gmail.com>
2023-07-05 10:06:26 -0500

    Add test for multi line sem token

--- test/lsp/sem-analysis.lisp
+++ test/lsp/sem-analysis.lisp
@@ -1,7 +1,10 @@
 (defpackage :alive/test/lsp/sem-analysis
     (:use :cl)
     (:export :run-all)
-    (:local-nicknames (:analysis :alive/lsp/sem-analysis)))
+    (:local-nicknames (:analysis :alive/lsp/sem-analysis)
+                      (:pos :alive/position)
+                      (:sem-types :alive/lsp/types/sem-tokens)
+                      (:token :alive/parse/token)))
 
 (in-package :alive/test/lsp/sem-analysis)
 
@@ -16,6 +19,21 @@
                           :actual (analysis::is-number "1/2/3/4"))))
 
 
+(defun test-add-token ()
+    (clue:test "Add token"
+        (let ((state (make-instance 'analysis::analysis-state))
+              (token (token:create :type-value nil
+                                   :start (pos:create 0 0)
+                                   :start-offset 0
+                                   :end (pos:create 2 3)
+                                   :end-offset 3
+                                   :text "foo")))
+            (analysis::add-sem-token state token sem-types:*comment*)
+            (clue:check-equal :expected 3
+                              :actual (length (analysis::sem-tokens state))))))
+
+
 (defun run-all ()
     (clue:suite "Semantic Analysis Tests"
-        (test-is-number)))
+        (test-is-number)
+        (test-add-token)))
