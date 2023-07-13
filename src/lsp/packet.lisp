867f7908c5065879abd8be775e132188bf99aea7
Rich Heller <rellehr@gmail.com>
2023-07-13 17:37:41 -0500

    Remove unused code

--- src/lsp/packet.lisp
+++ src/lsp/packet.lisp
@@ -15,54 +15,6 @@
                          :initarg :content-length)))
 
 
-(defclass packet ()
-        ((header :accessor header
-                 :initform nil
-                 :initarg :header)
-         (payload :accessor payload
-                  :initform nil
-                  :initarg :payload)))
-
-
-(defclass payload ()
-        ((jsonrpc :accessor jsonrpc
-                  :initform "2.0"
-                  :initarg :jsonrpc)
-         (id :accessor id
-             :initform nil
-             :initarg :id)))
-
-
-(defclass request-payload (payload)
-        ((method :accessor method-name
-                 :initform nil
-                 :initarg :method)
-         (params :accessor params
-                 :initform nil
-                 :initarg :params)))
-
-
-(defclass result-payload (payload)
-        ((result :accessor result
-                 :initform nil
-                 :initarg :result)))
-
-
-(defclass error-payload (payload)
-        ((error :accessor error-info
-             :initform nil
-             :initarg :error)))
-
-
-(defclass message ()
-        ((header :accessor header
-                 :initform (make-instance 'message-header)
-                 :initarg :header)
-         (payload :accessor payload
-                  :initform nil
-                  :initarg :payload)))
-
-
 #+win32
 (defvar end-line (format nil "~C" #\newline))
 
