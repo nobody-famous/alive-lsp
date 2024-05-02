b9864d21a9e08ec49869f6fa29a81d7881557638
Rich Heller <rellehr@gmail.com>
2024-05-02 14:06:11 -0500

    Report number of passed/failed tests

--- clue/run.lisp
+++ clue/run.lisp
@@ -1,9 +1,24 @@
 (in-package :clue)
 
 
+(defstruct test-results
+    (passed 0 :type fixnum)
+    (failed 0 :type fixnum))
+
+
 (defmacro suite (label &body body)
-    `(progn (print-header ,label)
-            ,@body))
+    (let ((out (gensym)))
+        `(let ((,out (make-test-results)))
+             (print-header ,label)
+             ,@(loop :for expr :in body
+                     :collect `(let ((expr-result ,expr))
+                                   (when (test-results-p expr-result)
+                                         (incf (test-results-passed ,out) (test-results-passed expr-result))
+                                         (incf (test-results-failed ,out) (test-results-failed expr-result)))) :into exprs
+                     :finally (return exprs))
+             ;  (format T "Passed: ~A Failed: ~A~%" (test-results-passed ,out) (test-results-failed ,out))
+             (print-footer ,label ,out)
+             ,out)))
 
 
 (defmacro test (label &body body)
@@ -17,4 +32,5 @@
              (if result
                  (format nil "FAILED: ~A" result)
                  "OK")
-             ,label)))
+             ,label)
+         (make-test-results :passed (if result 0 1) :failed (if result 1 0))))
