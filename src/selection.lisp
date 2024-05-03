22902aa2e0c1f09534c37facbcaee1d3e7e6be3b
Rich Heller <rellehr@gmail.com>
2024-05-03 10:34:27 -0500

    Update selection tree walking logic

--- src/selection.lisp
+++ src/selection.lisp
@@ -38,6 +38,34 @@
                           (return out))))
 
 
+(declaim (ftype (function (cons pos:text-position) (or T cons)) find-form-for-pos))
+(defun find-form-for-pos (forms pos)
+    (first (remove-if-not (lambda (form)
+                              (in-range pos (gethash "start" form) (gethash "end" form)))
+                   forms)))
+
+
+(declaim (ftype (function ((or T cons) hash-table)) create-node))
+(defun create-node (parent form)
+    (list (cons :range (range:create (gethash "start" form) (gethash "end" form)))
+          (cons :parent parent)))
+
+
+(declaim (ftype (function (cons pos:text-position)) get-range-tree))
+(defun get-range-tree (forms pos)
+    (loop :with form := (find-form-for-pos forms pos)
+          :with node := nil
+
+          :while form
+
+          :do (setf node (create-node node form))
+              (setf form (if (gethash "kids" form)
+                             (find-form-for-pos (gethash "kids" form) pos)
+                             nil))
+
+          :finally (return node)))
+
+
 (defun list-of-position-p (data)
     (and (consp data)
          (every #'pos:position-p data)))
