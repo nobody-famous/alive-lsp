ff1f137eaef8811c77d79375c228dce51d184c15
Rich Heller <rellehr@gmail.com>
2024-05-10 15:11:51 -0500

    Document definition handler and tests

--- src/session/handler/document.lisp
+++ src/session/handler/document.lisp
@@ -1,6 +1,7 @@
 (defpackage :alive/session/handler/document
     (:use :cl)
-    (:export :completion)
+    (:export :completion
+             :definition)
     (:local-nicknames (:comps :alive/lsp/completions)
                       (:lsp-msg :alive/lsp/message/abstract)
                       (:state :alive/session/state)))
@@ -8,6 +9,7 @@
 (in-package :alive/session/handler/document)
 
 
+(declaim (ftype (function (cons) hash-table) completion))
 (defun completion (msg)
     (let* ((id (cdr (assoc :id msg)))
            (params (cdr (assoc :params msg)))
@@ -23,5 +25,26 @@
 
             (setf (gethash "isIncomplete" data) T)
             (setf (gethash "items" data) items)
+
+            (lsp-msg:create-response id :result-value data))))
+
+
+(declaim (ftype (function (cons) hash-table) definition))
+(defun definition (msg)
+    (let* ((id (cdr (assoc :id msg)))
+           (params (cdr (assoc :params msg)))
+           (doc (cdr (assoc :text-document params)))
+           (pos (cdr (assoc :position params)))
+           (uri (cdr (assoc :uri doc)))
+           (file-text (state:get-file-text uri))
+           (text (if file-text file-text ""))
+           (location (alive/lsp/definition:get-location :text text :pos pos))
+           (uri (first location))
+           (range (second location)))
+
+        (let ((data (make-hash-table :test #'equalp)))
+
+            (setf (gethash "uri" data) uri)
+            (setf (gethash "range" data) range)
 
             (lsp-msg:create-response id :result-value data))))
--- test/session/handler/document.lisp
+++ test/session/handler/document.lisp
@@ -21,6 +21,21 @@
             (clue:expect-fail (lambda () (doc:completion (list (cons :id 5))))))))
 
 
+(defun test-definition ()
+    (clue:suite "Definition Tests"
+        (clue:test "Request"
+            (state:with-state (state:create)
+                (let ((resp (doc:definition (list (cons :id 1)
+                                                  (cons :params (list (cons :text-document (list (cons :uri "some/uri")))
+                                                                      (cons :position (list (cons :line 5)
+                                                                                            (cons :character 10)))))))))
+                    (clue:check-exists (gethash "result" resp)))))
+
+        (clue:test "Failure"
+            (clue:expect-fail (lambda () (doc:definition (list (cons :id 5))))))))
+
+
 (defun run-all ()
     (clue:suite "Document Handler Tests"
-        (test-completion)))
+        (test-completion)
+        (test-definition)))
