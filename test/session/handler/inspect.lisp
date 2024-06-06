69b9325982961da5a57a3a0cd146a7ecb8e7f100
Rich Heller <rellehr@gmail.com>
2024-06-06 15:39:44 -0500

    More inspect tests

--- test/session/handler/inspect.lisp
+++ test/session/handler/inspect.lisp
@@ -2,19 +2,43 @@
     (:use :cl)
     (:export :run-all)
     (:local-nicknames (:deps :alive/deps)
-                      (:inspect :alive/session/handler/inspect)))
+                      (:inspect :alive/session/handler/inspect)
+                      (:inspector :alive/inspector)
+                      (:state :alive/session/state)))
 
 (in-package :alive/test/session/handler/inspect)
 
 
-(defun test-do-inspect ()
-    (clue:test "Do inspect"
-        (clue:expect-fail (lambda () (inspect:do-inspect (list (cons :id 5)
-                                                               (cons :params (list (cons :text "foo")))))))
+(defun fake-inspector (result)
+    (inspector:create :text "foo" :pkg "cl-user" :result result))
 
-        (deps:with-deps (deps:create)
-            (inspect:do-inspect (list (cons :id 5)
-                                      (cons :params (list (cons :text "foo"))))))))
+
+(defun test-do-inspect ()
+    (clue:suite "Do inspect"
+        (clue:test "Fail"
+            (clue:expect-fail (lambda () (inspect:do-inspect (list (cons :id 5)
+                                                                   (cons :params (list (cons :text "foo"))))))))
+
+        (clue:test "New inspect"
+            (deps:with-deps (deps:create)
+                (inspect:do-inspect (list (cons :id 5)
+                                          (cons :params (list (cons :text "foo")))))))
+
+        (clue:test "New result symbol"
+            (state:with-state (state:create)
+                (deps:with-deps (deps:create :eval-fn (lambda (str)
+                                                          (declare (ignore str))
+                                                          'foo))
+                    (inspect:do-inspect (list (cons :id 5)
+                                              (cons :params (list (cons :text "foo"))))))))
+
+        (clue:test "Old result"
+            (state:with-state (state:create)
+                (deps:with-deps (deps:create)
+                    (state:add-inspector 5 (fake-inspector 10))
+                    (inspect:do-inspect (list (cons :id 5)
+                                              (cons :params (list (cons :text "foo")
+                                                                  (cons :id 5))))))))))
 
 
 (defun run-all ()
