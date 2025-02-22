f6f5f633d182db228e266f4346dd763b445e3ea7
Rich Heller <rellehr@gmail.com>
2025-02-22 15:42:27 -0600

    Update compile handler tests

--- src/session/handler/compile.lisp
+++ src/session/handler/compile.lisp
@@ -2,6 +2,8 @@
     (:use :cl)
     (:export :file
              :load-file
+             :new-file
+             :new-load-file
              :new-try
              :try)
     (:local-nicknames (:deps :alive/deps)
@@ -34,7 +36,7 @@
         (deps:new-send-msg deps (utils:result (cdr (assoc :id msg)) "messages" msgs))))
 
 
-(declaim (ftype (function (list) null) try))
+(declaim (ftype (function (list) null) file))
 (defun file (msg)
     (let* ((id (cdr (assoc :id msg)))
            (params (cdr (assoc :params msg)))
@@ -49,6 +51,23 @@
                                         (deps:send-msg (notification:stderr data))))
 
         (deps:send-msg (lsp-msg:create-response id :result-value "OK"))))
+
+
+(declaim (ftype (function (deps:dependencies list) null) new-file))
+(defun new-file (deps msg)
+    (let* ((id (cdr (assoc :id msg)))
+           (params (cdr (assoc :params msg)))
+           (path (cdr (assoc :path params))))
+
+        (deps:new-do-compile deps path
+                             :stdin-fn (lambda ()
+                                           (threads:wait-for-input))
+                             :stdout-fn (lambda (data)
+                                            (deps:new-send-msg deps (notification:stdout data)))
+                             :stderr-fn (lambda (data)
+                                            (deps:new-send-msg deps (notification:stderr data))))
+
+        (deps:new-send-msg deps (lsp-msg:create-response id :result-value "OK"))))
 
 
 (declaim (ftype (function (list) null) load-file))
@@ -67,3 +86,21 @@
                                                     (deps:send-msg (notification:stderr data)))))))
 
         (deps:send-msg (utils:result id "messages" msgs))))
+
+
+(declaim (ftype (function (deps:dependencies list) null) new-load-file))
+(defun new-load-file (deps msg)
+    (let* ((id (cdr (assoc :id msg)))
+           (params (cdr (assoc :params msg)))
+           (path (cdr (assoc :path params)))
+           (msgs (deps:new-do-load deps path
+                               :stdin-fn (lambda ()
+                                             (threads:wait-for-input))
+                               :stdout-fn (lambda (data)
+                                              (when (assoc :show-stdout params)
+                                                    (deps:new-send-msg deps (notification:stdout data))))
+                               :stderr-fn (lambda (data)
+                                              (when (assoc :show-stderr params)
+                                                    (deps:new-send-msg deps (notification:stderr data)))))))
+
+        (deps:new-send-msg deps (utils:result id "messages" msgs))))
--- test/session/handler/compile.lisp
+++ test/session/handler/compile.lisp
@@ -10,34 +10,34 @@
 
 (defun test-try ()
     (clue:test "Try"
-        (let ((sent-msg nil))
-            (deps:with-deps (deps:create :send-msg (lambda (msg)
-                                                       (setf sent-msg msg)
-                                                       nil))
-                (compile:try (list (cons :id 5)))
-                (clue:check-exists (gethash "result" sent-msg))))))
+        (let* ((sent-msg nil)
+               (deps (deps:new-create :send-msg (lambda (msg)
+                                                    (setf sent-msg msg)
+                                                    nil))))
+            (compile:new-try deps (list (cons :id 5)))
+            (clue:check-exists (gethash "result" sent-msg)))))
 
 
 (defun test-file ()
     (clue:test "File"
-        (let ((sent-msg nil))
-            (deps:with-deps (deps:create :send-msg (lambda (msg)
-                                                       (setf sent-msg msg)
-                                                       nil))
-                (compile:file (list (cons :id 5)
-                                    (cons :params (list (cons :path "some/path")))))
-                (clue:check-exists (gethash "result" sent-msg))))))
+        (let* ((sent-msg nil)
+               (deps (deps:new-create :send-msg (lambda (msg)
+                                                    (setf sent-msg msg)
+                                                    nil))))
+            (compile:new-file deps (list (cons :id 5)
+                                         (cons :params (list (cons :path "some/path")))))
+            (clue:check-exists (gethash "result" sent-msg)))))
 
 
 (defun test-load ()
     (clue:test "Load"
-        (let ((sent-msg nil))
-            (deps:with-deps (deps:create :send-msg (lambda (msg)
-                                                       (setf sent-msg msg)
-                                                       nil))
-                (compile:load-file (list (cons :id 5)
-                                         (cons :params (list (cons :path "some/path")))))
-                (clue:check-exists (gethash "result" sent-msg))))))
+        (let* ((sent-msg nil)
+               (deps (deps:new-create :send-msg (lambda (msg)
+                                                    (setf sent-msg msg)
+                                                    nil))))
+            (compile:new-load-file deps (list (cons :id 5)
+                                              (cons :params (list (cons :path "some/path")))))
+            (clue:check-exists (gethash "result" sent-msg)))))
 
 
 (defun run-all ()
