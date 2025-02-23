1e81649c03dbee4db1c569832f4e047862c42dda
Rich Heller <rellehr@gmail.com>
2025-02-22 22:02:23 -0600

    Update message tests

--- test/session/message.lisp
+++ test/session/message.lisp
@@ -1,7 +1,8 @@
 (defpackage :alive/test/session/message
     (:use :cl)
     (:export :run-all)
-    (:local-nicknames (:handlers :alive/session/handlers)
+    (:local-nicknames (:deps :alive/deps)
+                      (:handlers :alive/session/handlers)
                       (:msg :alive/session/message)
                       (:state :alive/session/state)))
 
@@ -11,36 +12,46 @@
 (defun test-handle ()
     (clue:suite "Handle Tests"
         (clue:test "Request has handler"
-            (handlers:with-handlers (list (cons "foo" (lambda (msg) (declare (ignore msg)))))
+            (let ((state (state:create))
+                  (deps (deps:new-create))
+                  (handlers (list (cons "foo" (lambda (deps msg) (declare (ignore deps msg)))))))
                 (clue:check-equal :expected nil
-                                  :actual (hash-table-p (msg:handle (list (cons :id 1)
-                                                                          (cons :method "foo")))))))
+                                  :actual (hash-table-p (msg:new-handle deps state handlers (list (cons :id 1)
+                                                                                                  (cons :method "foo")))))))
 
         (clue:test "Request no handler"
-            (clue:check-equal :expected T
-                              :actual (hash-table-p (msg:handle (list (cons :id 5)
-                                                                      (cons :method "foo"))))))
+            (let ((state (state:create))
+                  (deps (deps:new-create))
+                  (handlers (list (cons "foo" (lambda (deps msg) (declare (ignore deps msg)))))))
+                (clue:check-equal :expected T
+                                  :actual (hash-table-p (msg:new-handle deps state handlers (list (cons :id 1)
+                                                                                                  (cons :method "bar")))))))
 
         (clue:test "Invalid Message"
-            (clue:check-equal :expected T
-                              :actual (hash-table-p (msg:handle (list (cons :id 5))))))
+            (let ((state (state:create))
+                  (deps (deps:new-create))
+                  (handlers (list (cons "foo" (lambda (deps msg) (declare (ignore deps msg)))))))
+                (clue:check-equal :expected T
+                                  :actual (hash-table-p (msg:new-handle deps state handlers (list (cons :id 5)))))))
 
         (clue:test "Response has handler, no callback"
-            (handlers:with-handlers (list (cons "foo" (lambda (msg) (declare (ignore msg)))))
-                (state:with-state (state:create)
-                    (clue:check-equal :expected T
-                                      :actual (hash-table-p (msg:handle (list (cons :id 1)
-                                                                              (cons :result "foo"))))))))
+            (let ((state (state:create))
+                  (deps (deps:new-create))
+                  (handlers (list (cons "foo" (lambda (deps msg) (declare (ignore deps msg)))))))
+                (clue:check-equal :expected T
+                                  :actual (hash-table-p (msg:new-handle deps state handlers (list (cons :id 1)
+                                                                                                  (cons :result "foo")))))))
 
         (clue:test "Response has handler, has callback"
-            (handlers:with-handlers (list (cons "foo" (lambda (msg) (declare (ignore msg)))))
-                (state:with-state (state:create)
-                    (state:set-sent-msg-callback 1 (lambda (msg)
-                                                       (declare (ignore msg))
-                                                       (make-hash-table)))
-                    (clue:check-equal :expected T
-                                      :actual (hash-table-p (msg:handle (list (cons :id 1)
-                                                                              (cons :error "foo"))))))))))
+            (let ((state (state:create))
+                  (deps (deps:new-create))
+                  (handlers (list (cons "foo" (lambda (deps msg) (declare (ignore deps msg)))))))
+                (state:new-set-sent-msg-callback state 1 (lambda (msg)
+                                                             (declare (ignore msg))
+                                                             (make-hash-table)))
+                (clue:check-equal :expected T
+                                  :actual (hash-table-p (msg:new-handle deps state handlers (list (cons :id 1)
+                                                                                                  (cons :error "foo")))))))))
 
 
 (defun run-all ()
