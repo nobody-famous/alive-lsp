9bca2a6e2bffa590bbbf041acaf2f5aa58410c0c
Rich Heller <rellehr@gmail.com>
2025-03-14 13:00:01 -0500

    Incremental change

--- src/lsp/sig-help.lisp
+++ src/lsp/sig-help.lisp
@@ -30,10 +30,11 @@
 
 (declaim (ftype (function ((or null hash-table) (or null hash-table) (or null hash-table)) (values (or null string) &optional)) get-fn-package))
 (defun get-fn-package (token1 token2 token3)
-    (when (and (eq (token:get-type-value token1) types:*symbol*)
-               (eq (token:get-type-value token2) types:*colons*)
-               (eq (token:get-type-value token3) types:*symbol*))
-          (token:get-text token3)))
+    (if (and (eq (token:get-type-value token1) types:*symbol*)
+             (eq (token:get-type-value token2) types:*colons*)
+             (eq (token:get-type-value token3) types:*symbol*))
+        (token:get-text token3)
+        (package-name *package*)))
 
 
 (declaim (ftype (function ((or null hash-table)) (values (or null string) &optional)) get-fn-name))
@@ -42,15 +43,45 @@
           (token:get-text token)))
 
 
+(declaim (ftype (function (string string) string) generate-label))
+(defun generate-label (pkg-name fn-name)
+    (loop :with lambda-list := (symbols:get-lambda-list fn-name pkg-name)
+          :with label := fn-name
+          :with params := ()
+          :with index := (length label)
+          :with add-params := T
+
+          :for item :in lambda-list
+          :do (when (char= #\& (char (string item) 0))
+                    (setf add-params nil))
+              (when add-params
+                    (setf params (push (list (+ 1 index)
+                                             (+ 1 index (length (string item))))
+                                       params))
+                    (setf index (+ 1 index (length (string item)))))
+              (setf label (format nil "~A ~A" label item))
+
+          :finally (return (values label (reverse params)))))
+
+
 (declaim (ftype (function ((or null hash-table) (or null hash-table) (or null hash-table)) (or null hash-table)) get-sig))
 (defun get-sig (token1 token2 token3)
     (let* ((pkg-name (get-fn-package token1 token2 token3))
            (fn-name (get-fn-name token1))
-           (lambda-list (symbols:get-lambda-list fn-name pkg-name)))
-        (format T "***** GET-SIG ~A ~A ~A~%" fn-name lambda-list (type-of lambda-list))))
+           (doc (documentation (symbols:lookup fn-name pkg-name) 'function))
+           (info (make-hash-table)))
+        (multiple-value-bind (label params)
+                (generate-label pkg-name fn-name)
+            (setf (gethash "label" info) label)
+            (setf (gethash "documentation" info) doc)
+            (setf (gethash "parameters" info) params)
+            (setf (gethash "activeParameter" info) 0)
+
+            (alive/test/utils:print-hash-table "***** INFO" info)
+            info)))
 
 
-(declaim (ftype (function (&key (:text string) (:pos pos:text-position)) (or null cons)) signatures))
+(declaim (ftype (function (&key (:text string) (:pos pos:text-position)) (values (or null cons) &optional)) signatures))
 (defun signatures (&key text pos)
     (let* ((forms (forms:from-stream-or-nil (make-string-input-stream text)))
            (tokens (tokenizer:from-stream (make-string-input-stream text)))
@@ -64,11 +95,8 @@
            (pkg (pkgs:lookup pkg-name))
            (*package* (if pkg pkg *package*)))
 
-        (list (get-sig-info "foo x y")
-              (get-sig-info "foo a b"))
-
+        #+n (list (get-sig-info "foo x y"))
         (when (>= (length name-tokens) 3)
               (destructuring-bind (token1 token2 token3)
                       name-tokens
-                  (get-sig token1 token2 token3)
-                  nil))))
+                  (list (get-sig token1 token2 token3))))))
