5d2f4bc8dfadab6befd64ab57923b66137a327e6
Francis St-Amour <fstamour@users.noreply.github.com>
2026-02-10 15:30:28 -0500

    fix non-portable shebang, and use the proper way to load a system file (#91)
    * fix: use portable shebang for bash scripts
    
    * load alive-lsp.asd using asdf:load-asd in the shell scripts
    
    * alive-lsp.asdf: use "pathname" to reduce the indentation

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 sbcl \
     --noinform \
     --non-interactive \
-    --load alive-lsp.asd \
+    --eval "(require 'asdf)" \
+    --eval "(asdf:load-asd (truename \"alive-lsp.asd\"))" \
     --eval "(asdf:load-system \"alive-lsp/test\")" \
     --eval "(alive/test/coverage:run)"
